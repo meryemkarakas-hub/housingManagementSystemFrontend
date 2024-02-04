596a7c81f5c1cb592cca6ce5b2dd97ee800de0f7
meryem.karakas <meryemkarakas11@gmail.com>
2024-02-04 23:23:00 +0300

    İlk commit

---
+++ src/Activation.js
@@ -0,0 +1,11 @@
+import React, { Component } from 'react'
+
+export default class Activation extends Component {
+  render() {
+    return (
+      <div>
+         <h2>Activation Component</h2>
+      </div>
+    )
+  }
+}
---
+++ src/ResetPassword.js
@@ -0,0 +1,11 @@
+import React, { Component } from 'react'
+
+export default class ResetPassword extends Component {
+  render() {
+    return (
+      <div>
+        <h2>ResetPassword Component</h2>
+      </div>
+    )
+  }
+}
