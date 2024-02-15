6a6f5303f955c7bcfaef237fe250759ed134a166
meryem.karakas@hmb.gov.tr <meryem.karakas@hmb.gov.tr>
2024-02-15 11:33:40 +0300

    Sayfanın backend ile bağlantısı yapıldı.

--- src/Login.js
+++ src/Login.js
@@ -33,48 +33,24 @@ const Login = () => {
   const handleMouseDownPassword = (event) => {
     event.preventDefault();
   };
-  const validatePassword = (password) => {
-    if (password.length < 8 || password.length > 16) {
-      return false;
-    }
-
-    if (!/[A-Z]/.test(password)) {
-      return false;
-    }
-
-    if (!/[a-z]/.test(password)) {
-      return false;
-    }
-
-    if (!/[0-9]/.test(password)) {
-      return false;
-    }
-
-    if (!/[^A-Za-z0-9]/.test(password)) {
-      return false;
-    }
-
-    return true;
-  };
 
   const handlePasswordChange = (e) => {
     const newPassword = e.target.value;
     setPassword(newPassword.slice(0, 16));
-    setPasswordError(false); 
+    setPasswordError(false);
   };
 
   const handleIdentityNumberChange = (e) => {
     const value = e.target.value;
     if (/^\d{0,11}$/.test(value)) {
       setIdentityNumber(value);
-      setIdentityError(false); 
+      setIdentityError(false);
     }
   };
 
   const [snackbarOpen, setSnackbarOpen] = useState(false);
   const [snackbarMessage, setSnackbarMessage] = useState("");
   const [snackbarSeverity, setSnackbarSeverity] = useState("info");
-
 
   const handleSnackbarClose = () => {
     setSnackbarOpen(false);
@@ -87,46 +63,48 @@ const Login = () => {
   };
 
   const handleSubmit = async () => {
-    if (!identityNumber) {
+    if (!identityNumber && !password) {
       setIdentityError(true);
-      showSnackbar("TC kimlik numarası alanı boş bırakılamaz.", 0);
-    } else {
-      setIdentityError(false);
-    }
-  
-    if (!password) {
       setPasswordError(true);
-      showSnackbar("Şifre alanı boş bırakılamaz.", 0);
-    } else {
-      setPasswordError(false);
+      return;
+    } else if (!identityNumber) {
+      setIdentityError(true);
+      return;
+    } else if (!password) {
+      setPasswordError(true);
+      return;
+    } else if (identityNumber.length !== 11) {
+      setIdentityError(true);
+      showSnackbar("TC kimlik numaranız 11 haneden oluşmalıdır.", 0);
+      return;
     }
-  
-    if (identityNumber && password) {
-      try {
-        const response = await axios.post(
-          "http://localhost:8080/api/user/login",
-          {
-            identityNumber,
-            password,
-          }
-        );
-        
-        const { message, status } = response.data;
-        
-        if (status === "success") {
-          console.log(message);
-          showSnackbar(message, 1); 
-        } else {
-          console.error(message);
-          showSnackbar(message, 0); 
+
+    setIdentityError(false);
+    setPasswordError(false);
+
+    try {
+      const response = await axios.post(
+        "http://localhost:8080/api/user/login",
+        {
+          identityNumber,
+          password,
         }
-      } catch (error) {
-        console.error("Error occurred:", error);
-        showSnackbar("Giriş yapılırken bir hata oluştu.", 0);
+      );
+
+      const { message, status } = response.data;
+
+      if (status === 1) {
+        console.log(message);
+        showSnackbar(message, 1);
+      } else {
+        console.error(message);
+        showSnackbar(message, 0);
       }
+    } catch (error) {
+      console.error("Error occurred:", error);
+      showSnackbar("Giriş yapılırken bir hata oluştu.", 0);
     }
   };
-  
 
   const navigate = useNavigate();
 
@@ -138,11 +116,9 @@ const Login = () => {
     navigate("/sign-up");
   };
 
-
-
   return (
-  <>
-   <Snackbar
+    <>
+      <Snackbar
         anchorOrigin={{ vertical: "top", horizontal: "right" }}
         open={snackbarOpen}
         autoHideDuration={6000}
@@ -152,103 +128,109 @@ const Login = () => {
           {snackbarMessage}
         </Alert>
       </Snackbar>
-    <Box
-      sx={{
-        display: "flex",
-        flexDirection: "column",
-        alignItems: "center",
-        "& > :not(style)": {
-          m: 1,
-          width: 400,
-          height: 550,
-          p: 2,
-        },
-      }}
-    >
-      <Paper elevation={3}>
-        <Typography
-          variant="h4"
-          fontWeight="bold"
-          align="center"
-          style={{ marginTop: "40px", marginBottom: "40px" }}
-        >
-          Giriş Yap
-        </Typography>
-        <TextField
-          required
-          sx={{ m: 1, minWidth: 350 }}
-          helperText={
-            identityError ? "TC kimlik numarası alanı zorunludur." : ""
-          }
-          error={identityError}
-          id="demo-helper-text-misaligned"
-          label="TC Kimlik Numarası"
-          value={identityNumber}
-          onChange={handleIdentityNumberChange}
-        />
-        <FormControl sx={{ m: 1, minWidth: 350 }} variant="outlined">
-          <InputLabel
-            htmlFor="outlined-adornment-password"
-            style={{ color: passwordError ? "#dc143c" : "#616161" }}
+      <Box
+        sx={{
+          display: "flex",
+          flexDirection: "column",
+          alignItems: "center",
+          "& > :not(style)": {
+            m: 1,
+            width: 400,
+            height: 550,
+            p: 2,
+          },
+        }}
+      >
+        <Paper elevation={3}>
+          <Typography
+            variant="h4"
+            fontWeight="bold"
+            align="center"
+            style={{ marginTop: "40px", marginBottom: "40px" }}
           >
-            Şifre *
-          </InputLabel>
-          <OutlinedInput
+            Giriş Yap
+          </Typography>
+          <TextField
             required
-            id="outlined-adornment-password"
-            type={showPassword ? "text" : "password"}
-            endAdornment={
-              <InputAdornment position="end">
-                <IconButton
-                  aria-label="toggle password visibility"
-                  onClick={handleClickShowPassword}
-                  onMouseDown={handleMouseDownPassword}
-                  edge="end"
-                >
-                  {showPassword ? <VisibilityOff /> : <Visibility />}
-                </IconButton>
-              </InputAdornment>
+            sx={{ m: 1, minWidth: 350 }}
+            helperText={
+              identityError ? "TC kimlik numarası alanı zorunludur." : ""
             }
-            label="Şifre"
-            value={password}
-            onChange={handlePasswordChange}
-            error={passwordError}
-            inputProps={{
-              style: { color: "gray" },
-            }}
+            error={identityError}
+            id="demo-helper-text-misaligned"
+            label="TC Kimlik Numarası"
+            value={identityNumber}
+            onChange={handleIdentityNumberChange}
+            autoComplete="off"
           />
-          <FormHelperText error={passwordError}>
-            {passwordError ? "Şifre alanı zorunludur." : ""}
-          </FormHelperText>
-        </FormControl>
-        <Checkbox />
-        <span
-          style={{ color: "gray", marginLeft: "5px", marginRight: "100px" }}
-        >
-          Beni Hatırla
-        </span>
-        <Link variant="body2" onClick={handleResetPasswordClick}>
-          {"Şifremi Unuttum"}
-        </Link>
-        <Button
-          variant="contained"
-          sx={{ m: 1, minWidth: 350, textTransform: "none" }}
-          onClick={handleSubmit}
-        >
-          Giriş Yap
-        </Button>
-        <div
-          style={{ display: "flex", alignItems: "center", marginLeft: "10px" }}
-        >
-          <p style={{ color: "grey", marginRight: "5px", marginTop: "15px" }}>
-            Üyeliğiniz yok mu?
-          </p>
-          <Link variant="body2" onClick={handleSignUpClick}>
-            Kaydol
+          <FormControl sx={{ m: 1, minWidth: 350 }} variant="outlined">
+            <InputLabel
+              htmlFor="outlined-adornment-password"
+              style={{ color: passwordError ? "#dc143c" : "#616161" }}
+            >
+              Şifre *
+            </InputLabel>
+            <OutlinedInput
+              required
+              id="outlined-adornment-password"
+              type={showPassword ? "text" : "password"}
+              endAdornment={
+                <InputAdornment position="end">
+                  <IconButton
+                    aria-label="toggle password visibility"
+                    onClick={handleClickShowPassword}
+                    onMouseDown={handleMouseDownPassword}
+                    edge="end"
+                  >
+                    {showPassword ? <VisibilityOff /> : <Visibility />}
+                  </IconButton>
+                </InputAdornment>
+              }
+              label="Şifre"
+              value={password}
+              onChange={handlePasswordChange}
+              error={passwordError}
+              inputProps={{
+                style: { color: "gray" },
+              }}
+              autoComplete="off"
+            />
+            <FormHelperText error={passwordError}>
+              {passwordError ? "Şifre alanı zorunludur." : ""}
+            </FormHelperText>
+          </FormControl>
+          <Checkbox />
+          <span
+            style={{ color: "gray", marginLeft: "5px", marginRight: "100px" }}
+          >
+            Beni Hatırla
+          </span>
+          <Link variant="body2" onClick={handleResetPasswordClick}>
+            {"Şifremi Unuttum"}
           </Link>
-        </div>
-      </Paper>
-    </Box>
+          <Button
+            variant="contained"
+            sx={{ m: 1, minWidth: 350, textTransform: "none" }}
+            onClick={handleSubmit}
+          >
+            Giriş Yap
+          </Button>
+          <div
+            style={{
+              display: "flex",
+              alignItems: "center",
+              marginLeft: "10px",
+            }}
+          >
+            <p style={{ color: "grey", marginRight: "5px", marginTop: "15px" }}>
+              Üyeliğiniz yok mu?
+            </p>
+            <Link variant="body2" onClick={handleSignUpClick}>
+              Kaydol
+            </Link>
+          </div>
+        </Paper>
+      </Box>
     </>
   );
 };
