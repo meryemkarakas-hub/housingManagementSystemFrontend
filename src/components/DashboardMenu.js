4469a61a4df6d6f264aa72664db2d71975a7e65e
meryem.karakas@hmb.gov.tr <meryem.karakas@hmb.gov.tr>
2024-02-27 17:34:52 +0300

    Güncellemeler yapıldı.

--- src/components/DashboardMenu.js
+++ src/components/DashboardMenu.js
@@ -12,9 +12,10 @@ import Button from '@mui/material/Button';
 import Tooltip from '@mui/material/Tooltip';
 import MenuItem from '@mui/material/MenuItem';
 import AdbIcon from '@mui/icons-material/Adb';
+import ApartmentIcon from '@mui/icons-material/Apartment';
 
-const pages = ['Products', 'Pricing', 'Blog'];
-const settings = ['Profile', 'Account', 'Dashboard', 'Logout'];
+const pages = ['Ana Sayfa', 'Hakkımızda', 'İletişim'];
+const settings = ['TC Kimlik Numarası', 'E-posta Adresi', 'Cep Telefonu', 'Rol Seç','Profil','Çıkış Yap'];
 
 function ResponsiveAppBar() {
   const [anchorElNav, setAnchorElNav] = React.useState(null);
@@ -39,7 +40,7 @@ function ResponsiveAppBar() {
     <AppBar position="static">
       <Container maxWidth="xl">
         <Toolbar disableGutters>
-          <AdbIcon sx={{ display: { xs: 'none', md: 'flex' }, mr: 1 }} />
+          <ApartmentIcon sx={{ display: { xs: 'none', md: 'flex' }, mr: 1 }} />
           <Typography
             variant="h6"
             noWrap
@@ -55,7 +56,7 @@ function ResponsiveAppBar() {
               textDecoration: 'none',
             }}
           >
-            LOGO
+            YÖNETİM SİSTEMLERİ
           </Typography>
 
           <Box sx={{ flexGrow: 1, display: { xs: 'flex', md: 'none' } }}>
